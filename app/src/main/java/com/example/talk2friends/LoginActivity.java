ee1182b4f4224f54c75c7d4a0933746c8f1ee581
Hyunmin Kim <hyunmink@usc.edu>
2023-10-29 12:21:34 -0700

    More changes to Sign up and Log In UI

---
+++ app/src/main/java/com/example/talk2friends/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.talk2friends;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView email_tv;
+    TextView password_tv;
+    String email;
+    String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email_tv = (TextView) findViewById(R.id.email);
+        password_tv = (TextView) findViewById(R.id.password);
+        TextView login = (TextView) findViewById(R.id.login);
+        TextView signup = (TextView) findViewById(R.id.sign_up);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                email = email_tv.getText().toString();
+                password = password_tv.getText().toString();
+                System.out.println("Email is: " + email + "\n");
+                System.out.println("Password is: " + password + "\n");
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
