958ad814a6be547dc367be02c6499113fa13409e
Gabriel Do <gtdo@usc.edu>
2023-10-30 12:39:27 -0700

    added activities for friends, main, profile, profielCreation, CreateMeeting; however only completed UI for profile and profileCreation

---
+++ app/src/main/java/com/example/talk2friends/ProfileActivity.java
@@ -0,0 +1,37 @@
+package com.example.talk2friends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        String nameText = ""; // get from database?
+        String ageText = "";
+        String affiliationText = "";
+
+        TextView nameView = (TextView)findViewById(R.id.name_text);
+        TextView ageView = (TextView)findViewById(R.id.age_text);
+        TextView affiliationView = (TextView)findViewById(R.id.affiliation_text);
+
+        nameView.setText(nameText);
+        ageView.setText(ageText);
+        affiliationView.setText(affiliationText);
+
+        TextView v = (TextView) findViewById(R.id.button);
+        v.setOnClickListener(this::toProfileCreation);
+    }
+
+    public void toProfileCreation(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+}
