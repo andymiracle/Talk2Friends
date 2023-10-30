d08a77539e017cc9bbf1af337c6e5a4a5702106d
Gabriel Do <gtdo@usc.edu>
2023-10-30 13:11:40 -0700

    edit create meeting and profile creation

--- app/src/main/java/com/example/talk2friends/CreateMeetingActivity.java
+++ app/src/main/java/com/example/talk2friends/CreateMeetingActivity.java
@@ -3,6 +3,8 @@ package com.example.talk2friends;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CreateMeetingActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class CreateMeetingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meeting);
+
+        TextView v = (TextView) findViewById(R.id.submit); // submit
+        v.setOnClickListener(this::onClickSubmit);
+    }
+
+    public void onClickSubmit(View view){
+
     }
 }
