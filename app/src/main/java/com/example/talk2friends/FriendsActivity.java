c6dad9305b95c9ae02dae66e1faa228ad301b07f
Hyunmin Kim <hyunmink@usc.edu>
2023-10-30 15:14:09 -0700

    added more activity files for adding friends and inviting friends

--- app/src/main/java/com/example/talk2friends/FriendsActivity.java
+++ app/src/main/java/com/example/talk2friends/FriendsActivity.java
@@ -2,13 +2,37 @@ package com.example.talk2friends;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FriendsActivity extends AppCompatActivity {
+    TextView add_tv;
+    TextView invite_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        add_tv = (TextView) findViewById(R.id.add_friends);
+        invite_tv = (TextView) findViewById(R.id.invite_friends);
+
+        add_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FriendsActivity.this, AddFriendsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        invite_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FriendsActivity.this, InviteFriendsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
