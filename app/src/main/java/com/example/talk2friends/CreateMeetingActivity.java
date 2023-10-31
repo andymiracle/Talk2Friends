715c42ae990a9488ac5a03b0d94e04f7a54569ea
Gabriel Do <gtdo@usc.edu>
2023-10-30 19:18:21 -0700

    eidts to profile edit/creation and meeting

--- app/src/main/java/com/example/talk2friends/CreateMeetingActivity.java
+++ app/src/main/java/com/example/talk2friends/CreateMeetingActivity.java
@@ -5,25 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class CreateMeetingActivity extends AppCompatActivity {
+
+    String conversationTopic;
+    String time;
+    String location;
+
+    TextView conversationTopicView;
+    TextView timeView;
+    TextView locationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meeting);
 
-        TextView v = (TextView) findViewById(R.id.submit); // submit
-        v.setOnClickListener(this::onClickSubmit);
+        TextView v = (TextView) findViewById(R.id.create); // submit
+        v.setOnClickListener(this::onClickCreate);
     }
 
-    public void onClickSubmit(View view){
+    public void onClickCreate(View view){
 
-        // add meeting to database
+        Boolean valid = false;
 
-        // go back to main meetings page
-        Intent intent = new Intent(this, MainMeetingActivity.class);
-        startActivity(intent);
+        valid = true;
+
+        if (valid) {
+            // add meeting to database
+            // go back to main meetings page
+            Intent intent = new Intent(this, MainMeetingActivity.class);
+            startActivity(intent);
+        } else {
+            TextView error_tv = (TextView) findViewById(R.id.error);
+            error_tv.setText("One or more fields are empty");
+        }
+
     }
 }
--- app/src/main/java/com/example/talk2friends/ProfileCreationActivity.java
+++ app/src/main/java/com/example/talk2friends/ProfileCreationActivity.java
@@ -48,7 +48,7 @@ public class ProfileCreationActivity extends AppCompatActivity {
         // Specify the layout to use when the list of choices appears.
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner.
-        spinner2.setAdapter(adapter);
+        spinner2.setAdapter(adapter2);
 
         TextView v = (TextView) findViewById(R.id.submit); // submit
         v.setOnClickListener(this::onClickSubmit);
